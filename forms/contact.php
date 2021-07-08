1f26eaa4843a20d0d446b257b46c54509120ba44
Prasansha Satpathy <56639527+Sara-cos@users.noreply.github.com>
2021-07-08 22:25:57 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -2,11 +2,11 @@
   
   $receiving_email_address = 'prasansha.satpathy@gmail.com';
 
-  if(file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+//  if(file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+//     include( $php_email_form );
+//   } else {
+//     die( 'Unable to load the "PHP Email Form" Library!');
+//   } 
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
